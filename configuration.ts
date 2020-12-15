06cb598968046c93d014acaf534fceb14742cb66
Vladimir Tyrin <vtyrin@in-technology.de>
2020-12-15 11:04:56 +0700

    new config

--- configuration.ts
+++ configuration.ts
@@ -1,16 +1,12 @@
-export const baseDomainName = '192kb.ru';
+export const baseDomainName = 'levsha.work';
 export const basePath = '/levsha-api';
 export const serverPort = 3001;
 export const productionPort = 3001;
 export const serverApi = 'https://' + baseDomainName + basePath;
 export const productionHomeURL = 'https://' + baseDomainName + basePath;
 export const minPassword_length = 4;
-export const allowedOrigins = [
-  'http://localhost:3000',
-  'https://dev.' + baseDomainName,
-  'https://' + baseDomainName,
-];
+export const allowedOrigins = ['https://' + baseDomainName];
 export const cookieMaxAge = 3600000;
 export const brand = 'Levsha';
-export const uploadsPath = __dirname + '/../../html';
+export const uploadsPath = __dirname + '/../html';
 export const uploadsRelativePath = '/uploads/';
